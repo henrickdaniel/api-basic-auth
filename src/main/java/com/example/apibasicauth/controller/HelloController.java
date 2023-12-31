23809d85934997a5298fc1dfa0a2cc109e7e177c
Henrick Daniel Soares <henrickdaniel@gmail.com>
2023-12-31 14:56:57 -0300

    Add return type and url port

--- src/main/java/com/example/apibasicauth/controller/HelloController.java
+++ src/main/java/com/example/apibasicauth/controller/HelloController.java
@@ -2,17 +2,28 @@ package com.example.apibasicauth.controller;
 
 import br.com.henrick.HelloRepresentation;
 import br.com.henrick.V1Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 
 @RestController
 public class HelloController implements V1Api {
 
-
-    @Override
-    @GetMapping("/v1/hello")
+    @Operation(summary = "getHello", description = "", security = {
+            @SecurityRequirement(name = "basicAuth")    }, tags={ "hello-controller" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = "application/json", schema = @Schema(implementation = HelloRepresentation.class))) })
+    @RequestMapping(value = "/v1/hello",
+            produces = { "application/json" },
+            method = RequestMethod.GET)
     public ResponseEntity<HelloRepresentation> hello() throws RestClientException {
         HelloRepresentation helloRepresentation = new HelloRepresentation();
         helloRepresentation.setMessage("Hello");
